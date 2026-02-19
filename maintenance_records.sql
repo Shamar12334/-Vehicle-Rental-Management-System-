0cda8144f3f27e1b8958ca51a3aa5bbaa78758de
shamar12334 <weekesshamar38@gmail.com>
2026-02-19 16:58:50 -0500

    created records table

---
+++ maintenance_records.sql
@@ -0,0 +1,15 @@
+CREATE TABLE records(
+	date: int,
+	time: DATETIME,
+	vehicle: INTEGER PRIMARY_KEY,
+	reporting_customer: INTEGER PRIMARY KEY,
+	issue_type: String("rountine","urgent"),
+	description,
+	status,
+	resovled_date
+	resolve time
+	staff_assigned,
+	notes
+
+
+);
